2e8b56757332cabbe1e9ff7a512734b6b8ad5f91
Yuhao Zhang(Eric) <ericzhang041227@gmail.com>
2026-02-21 16:42:10 -0500

    Fix condition to count odd elements correctly

--- Task2_JUnit_Analysis/Program4.java
+++ Task2_JUnit_Analysis/Program4.java
@@ -16,7 +16,7 @@ public class Program4 {
 	 
 		 for (int i = 0; i < x.length; i++)
 		 {
-			 if (x[i]%2 == 1 || x[i] > 0)
+			 if (x[i]%2 != 0 || x[i] > 0)
 			 {
 				 count++;
 			 }
@@ -26,3 +26,4 @@ public class Program4 {
 		 // test: x=[-3, -2, 0, 1, 4] 
 		 // Expected = 3
 }
+
